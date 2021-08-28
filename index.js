c62aabd0876fe29b7bd35f05d2850076cd637d49
Brad R <brad.w.rollinson@gmail.com>
2021-08-28 15:16:26 -0400

    add switch case(s)

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const consoleTable = require('console.table')
 const mysql = require('mysql2');
 
 const db = mysql.createConnection(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
         host: 'localhost',
-        // MySQL username,
-        user: 'root',
-        // MySQL password
-        password: '1234',
-        database: ''
     },
 );
 
@@ -22,7 +20,35 @@ const mainPrompt = () => {
             type: 'list',
             name: 'main',
             message: 'What would you like to do?',
-            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Exit']
         }
     ])
+        .then(value => {
+            switch (value.main) {
+                case 'View All Employees':
+                    viewAllEmployees();
+                    break;
+                case 'Add Employee':
+                    addEmployee();
+                    break;
+                case 'Update Employee Role':
+                    updateEmployeeRole();
+                    break;
+                case 'View All Roles':
+                    viewAllRoles();
+                    break;
+                case 'Add Role':
+                    addRole();
+                    break;
+                case 'View All Departments':
+                    viewAllDepartments();
+                    break;
+                case 'Add Department':
+                    addDepartment();
+                    break;
+                case 'Exit':
+                    dbConnection.end();
+                    console.log('Thank you! Goodbye!')
+            }
+        });
 }
