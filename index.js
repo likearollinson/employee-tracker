e35af77f5e4f222e1047329484be3da958e1316b
Brad R <brad.w.rollinson@gmail.com>
2021-08-29 14:04:22 -0400

    update employees role function added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+// import and require modules
 const inquirer = require('inquirer');
 const consoleTable = require('console.table')
-// Import and require mysql2
 const mysql = require('mysql2');
+const promiseMySql = require('promise-mysql');
 
-
+// variable for connection criteria
 const dbConnectionProperties =
 {
     host: 'localhost',
@@ -15,6 +16,7 @@ const dbConnectionProperties =
 
 console.log('---------------------------\nEMPLOYEE MANAGER\n---------------------------')
 
+//main prompt for all functions
 const mainPrompt = () => {
     return inquirer.prompt([
         {
@@ -53,8 +55,11 @@ const mainPrompt = () => {
             }
         });
 }
+
+// variable for connecting to database
 const dbConnection = mysql.createConnection(dbConnectionProperties);
 
+// connect to database
 dbConnection.connect(err => {
     if (err) {
         throw err;
@@ -63,6 +68,7 @@ dbConnection.connect(err => {
     mainPrompt();
 })
 
+// function for viewing all employees in console
 function viewAllEmployees() {
     dbConnection.query("SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.name AS departments, roles.salary, CONCAT(e.first_name, ' ' ,e.last_name) AS manager FROM employees INNER JOIN roles ON roles.id = employees.role_id INNER JOIN departments ON departments.id = roles.department_id LEFT JOIN employees e ON employees.manager_id = e.id;",
         (err, results) => {
@@ -75,10 +81,12 @@ function viewAllEmployees() {
     );
 }
 
+// function for adding a new employee to database
 function addEmployee() {
     let managerArr = [];
     let roleArr = [];
 
+    // query for selecting managers from employees table
     dbConnection.query('SELECT first_name, last_name FROM employees WHERE manager_id IS NULL',
         (err, results) => {
             if (err) throw err;
@@ -88,11 +96,13 @@ function addEmployee() {
         }
     );
 
+    // query for selecting role titles from database
     dbConnection.query('SELECT * FROM roles', (err, results) => {
         if (err) throw err;
         results.map(roles => roleArr.push(`${roles.title}`));
         return roleArr;
     });
+    // prompt for entering new employee information
     inquirer
         .prompt([
             {
@@ -139,5 +149,65 @@ function addEmployee() {
         });
 }
 
+// function for updating employee role with salary and department
+function updateEmployeeRole() {
+    let employeeArr = [];
+    let roleArr = [];
 
+    promiseMySql
+        .createConnection(dbConnectionProperties)
+        .then(conn => {
+            return Promise.all([
+                conn.query('SELECT id, title FROM roles ORDER BY title ASC'),
+                conn.query("SELECT employees.id, CONCAT(employees.first_name, ' ', employees.last_name) AS Employee from employees ORDER BY Employee ASC"),
+            ]);
+        })
+        .then(([roles, employees]) => {
+            roles.map(roles => roleArr.push(roles.title));
+
+            employees.map(employees => employeeArr.push(employees.Employee));
+
+            return Promise.all([roles, employees]);
+        })
+        .then(([roles, employees]) => {
+            inquirer
+                .prompt([
+                    {
+                        type: 'list',
+                        message: "Which employee's role do you want to update?",
+                        name: 'employee',
+                        choices: employeeArr
+                    },
+                    {
+                        type: 'list',
+                        message: 'Which role would you like assigned to the employee?',
+                        name: 'role',
+                        choices: roleArr
+                    },
+                ])
+                .then(results => {
+                    let role_id;
+                    let employee_id;
+
+                    for (let i = 0; i < roles.length; i++) {
+                        if (results.role == roles[i].title) {
+                            role_id = roles[i].id;
+                        }
+                    }
+
+                    for (let i = 0; i < employees.length; i++) {
+                        if (results.employee == employees[i].Employee) {
+                            employee_id = employees[i].id;
+                        }
+                    }
+                    // update role values
+                    dbConnection.query(`UPDATE employees SET role_id = ${role_id} WHERE id = ${employee_id}`,
+                        (err, results) => {
+                            if (err) return err;
+                            mainPrompt();
+                        }
+                    );
+                });
+        });
+}
 
